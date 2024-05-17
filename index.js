f776994f8af03347b0610c1b5376a47239ecb07a
LauraGoan <168082148+LauraGoan@users.noreply.github.com>
2024-05-17 12:05:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function canvia_seccio(num_boto) {
 }
 let validat = false;    // variable que permet saber si hi ha algun usuari validat
 let nom, contrasenya;
-let scriptURL = "https://script.google.com/macros/s/AKfycbws3Czb6cc7kr6LCf08RsWCn36PzQrzyfUeIfwd_KspVo48pkOE0FBh0uLz63_f3B_zGA/exec"    // s'ha de substituir la cadena de text per la URL del script
+let scriptURL = "https://script.google.com/macros/s/AKfycbzh6L9OdL4D8HAklBcnP_dkuH26W26uJdfruIccHIMEn1oFsuIFiw8_sr_0W6F5KwhEfg/exec"    // s'ha de substituir la cadena de text per la URL del script
 
 function inici_sessio() {
     nom = document.getElementById("nom_usuari").value;    // la propietat "value" d'un quadre de text correspon al text escrit per l'usuari
